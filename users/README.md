1b839b852dd4898a74bf3818197653683e24f2b3
HirenPatel2791 <hiren.patel@scanbuy.com>
2018-03-28 16:39:58 -0400

    more ..

---
+++ users/README.md
@@ -0,0 +1,10 @@
+#users graphql
+
+`npm install --save express express-graphql graphql lodash`
+
+
+`npm install -save json-server`
+
+`npm run json:server`
+
+
--- users/schema/schema.js
+++ users/schema/schema.js
@@ -4,6 +4,7 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLList,
   GraphQLSchema
 } = graphql;
 
@@ -12,13 +13,37 @@ const {
 //   { id: '47', firstName: 'Hiren', age: 26}
 // ];
 
+const CompanyType = new GraphQLObjectType({
+  name:'Company',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+        .then (resp => resp.data )
+      }
+    }
+  })
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt}
-  }
+    age: { type: GraphQLInt},
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        //console.log(parentValue, args);
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+        .then(resp => resp.data);
+      }
+    }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -31,6 +56,14 @@ const RootQuery = new GraphQLObjectType({
         //return _.find(users, { id: args.id });
         return axios.get(`http://localhost:3000/users/${args.id}`)
           .then(resp => resp.data); // cause response is nested as data :{ response } in axios
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString }},
+      resolve(parentValue,args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+        .then (resp => resp.data);
       }
     }
   }
